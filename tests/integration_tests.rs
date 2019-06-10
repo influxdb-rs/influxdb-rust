d35a5684bbdd1ab1089106f1eeab2189d105cecf
Gero Gerke <11deutron11@gmail.com>
2019-06-10 15:31:39 +0200

    Install Influx on CI for Tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -36,7 +36,10 @@ fn test_write_field() {
     let client = create_client();
     let query = InfluxDbQuery::write_query("weather").add_field("temperature", 82);
     let result = get_runtime().block_on(client.query(query));
-    assert!(result.is_ok(), "Should be no error");
+    assert!(
+        result.is_ok(),
+        format!("Should be no error: {}", result.unwrap_err())
+    );
 }
 
 #[test]
@@ -47,7 +50,10 @@ fn test_read() {
     let client = create_client();
     let query = InfluxDbQuery::raw_read_query("SELECT * FROM weather");
     let result = get_runtime().block_on(client.query(query));
-    assert!(result.is_ok(), "Should be no error");
+    assert!(
+        result.is_ok(),
+        format!("Should be no error: {}", result.unwrap_err())
+    );
     assert!(
         !result.unwrap().contains("error"),
         "Data contained a database error"
@@ -72,5 +78,8 @@ fn test_json_query() {
     let query = InfluxDbQuery::raw_read_query("SELECT * FROM weather");
     let result = get_runtime().block_on(client.json_query::<Weather, _>(query));
 
-    assert!(result.is_ok(), "We could read from the DB");
+    assert!(
+        result.is_ok(),
+        format!("We couldn't read from the DB: {}", result.unwrap_err())
+    );
 }
