0734d38c2cfe2a5b80d260d0e4021f4f5a4cc54f
Dominic <git@msrd0.de>
2019-10-09 14:20:41 +0200

    basic derive stuff

---
+++ influxdb_derive/src/lib.rs
@@ -0,0 +1,12 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+
+mod writeable;
+use writeable::expand_writeable;
+
+#[proc_macro_derive(InfluxDbWriteable)]
+pub fn derive_writeable(tokens : TokenStream) -> TokenStream
+{
+	expand_writeable(tokens)
+}
