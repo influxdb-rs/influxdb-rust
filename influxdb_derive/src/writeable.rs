c2fd9a90e81b1f252bcfd8ab5e3c742e1c41106d
Dominic <git@msrd0.de>
2019-10-26 17:19:39 +0200

    first attempt at tags

--- influxdb_derive/src/writeable.rs
+++ influxdb_derive/src/writeable.rs
@@ -1,6 +1,33 @@
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, Fields, Ident, ItemStruct};
+use syn::{parse_macro_input, Field, Fields, Ident, ItemStruct};
+
+struct WriteableField {
+    ident: Ident,
+    is_tag: bool,
+}
+
+impl From<Field> for WriteableField {
+    fn from(field: Field) -> WriteableField {
+        let ident = field.ident.expect("fields without ident are not supported");
+        let is_tag = field
+            .attrs
+            .iter()
+            .filter(|attr| {
+                attr.path
+                    .segments
+                    .iter()
+                    .last()
+                    .map(|seg| seg.ident.to_string())
+                    .unwrap_or_default()
+                    == "tag"
+            })
+            .nth(0)
+            .is_some();
+        WriteableField { ident, is_tag }
+    }
+}
 
 pub fn expand_writeable(tokens: TokenStream) -> TokenStream {
     let input = parse_macro_input!(tokens as ItemStruct);
@@ -8,12 +35,21 @@ pub fn expand_writeable(tokens: TokenStream) -> TokenStream {
     let generics = input.generics;
 
     let time_field = format_ident!("time");
-    let fields: Vec<Ident> = match input.fields {
+	#[allow(clippy::cmp_owned)] // that's not how ident work clippy
+    let fields: Vec<TokenStream2> = match input.fields {
         Fields::Named(fields) => fields
             .named
             .into_iter()
-            .map(|field| field.ident.expect("fields without ident are not supported"))
-            .filter(|field| *field != *time_field.to_string())
+            .map(WriteableField::from)
+            .filter(|field| field.ident.to_string() != time_field.to_string())
+            .map(|field| {
+                let ident = field.ident;
+				#[allow(clippy::match_bool)]
+                match field.is_tag {
+                    true => quote!(query.add_field(stringify!(#ident), self.#ident)),
+                    false => quote!(query.add_field(stringify!(#ident), self.#ident)),
+                }
+            })
             .collect(),
         _ => panic!("a struct without named fields is not supported"),
     };
@@ -26,7 +62,7 @@ pub fn expand_writeable(tokens: TokenStream) -> TokenStream {
                 let timestamp : ::influxdb::Timestamp = self.#time_field;
                 let mut query = timestamp.into_query(name);
                 #(
-                    query = query.add_field(stringify!(#fields), self.#fields);
+                    query = #fields;
                 )*
                 query
             }
