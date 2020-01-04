da785fcfdcae348c130260ac2fc87fe2ae202ad4
Moritz Gunz <moritz.gunz@gmail.com>
2020-01-04 13:54:55 +0100

    Allow main functions in docs

--- src/lib.rs
+++ src/lib.rs
@@ -67,6 +67,8 @@
 //! [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 //!
 
+#![allow(clippy::needless_doctest_main)]
+
 #[macro_use]
 extern crate failure;
 
