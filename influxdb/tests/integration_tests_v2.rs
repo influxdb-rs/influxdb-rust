211e8ef1f379878e099de7a6a7187e4ada540337
Ning Sun <sunng@protonmail.com>
2021-03-27 21:53:33 +0800

    (test) remove unneeded v2 tests

--- influxdb/tests/integration_tests_v2.rs
+++ influxdb/tests/integration_tests_v2.rs
@@ -2,64 +2,10 @@ extern crate influxdb;
 
 #[path = "./utilities.rs"]
 mod utilities;
-use utilities::{assert_result_err, assert_result_ok, create_client, run_test};
+use utilities::{assert_result_err, assert_result_ok, run_test};
 
 use influxdb::InfluxDbWriteable;
 use influxdb::{Client, Error, Query, Timestamp};
-
-/// INTEGRATION TEST
-///
-/// This test case tests whether the InfluxDB server can be connected to and gathers info about it - tested with async_std
-#[async_std::test]
-#[cfg(not(tarpaulin_include))]
-async fn test_ping_influx_db_async_std() {
-    let client = create_client("notusedhere");
-    let result = client.ping().await;
-    assert_result_ok(&result);
-
-    let (build, version) = result.unwrap();
-    assert!(!build.is_empty(), "Build should not be empty");
-    assert!(!version.is_empty(), "Build should not be empty");
-
-    println!("build: {} version: {}", build, version);
-}
-
-/// INTEGRATION TEST
-///
-/// This test case tests whether the InfluxDB server can be connected to and gathers info about it * tested with tokio
-#[tokio::test]
-#[cfg(not(tarpaulin_include))]
-async fn test_ping_influx_db_tokio() {
-    let client = create_client("notusedhere");
-    let result = client.ping().await;
-    assert_result_ok(&result);
-
-    let (build, version) = result.unwrap();
-    assert!(!build.is_empty(), "Build should not be empty");
-    assert!(!version.is_empty(), "Build should not be empty");
-
-    println!("build: {} version: {}", build, version);
-}
-
-/// INTEGRATION TEST
-///
-/// This test case tests connection error
-#[async_std::test]
-#[cfg(not(tarpaulin_include))]
-async fn test_connection_error() {
-    let test_name = "test_connection_error";
-    let client = Client::new("http://127.0.0.1:10086", test_name).with_token("admintoken");
-    let read_query = <dyn Query>::raw_read_query("SELECT * FROM weather");
-    let read_result = client.query(&read_query).await;
-    assert_result_err(&read_result);
-    match read_result {
-        Err(Error::ConnectionError { .. }) => {}
-        _ => panic!(
-            "Should cause a ConnectionError: {}",
-            read_result.unwrap_err()
-        ),
-    }
-}
 
 /// INTEGRATION TEST
 ///
