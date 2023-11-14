dc160db4c645b70272ba5c83bebf4da1e44ec338
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-11-14 13:19:28 +0100

    README: use ?

--- influxdb/src/lib.rs
+++ influxdb/src/lib.rs
@@ -25,13 +25,12 @@
 //! For an example with using Serde deserialization, please refer to [serde_integration](crate::integrations::serde_integration)
 //!
 //! ```rust,no_run
-//! use influxdb::{Client, Timestamp, ReadQuery};
-//! use influxdb::InfluxDbWriteable;
 //! use chrono::{DateTime, Utc};
+//! use influxdb::{Client, Error, InfluxDbWriteable, ReadQuery, Timestamp};
 //!
 //! #[tokio::main]
 //! // or #[async_std::main] if you prefer
-//! async fn main() {
+//! async fn main() -> Result<(), Error> {
 //!     // Connect to db `test` on `http://localhost:8086`
 //!     let client = Client::new("http://localhost:8086", "test");
 //!
@@ -39,35 +38,34 @@
 //!     struct WeatherReading {
 //!         time: DateTime<Utc>,
 //!         humidity: i32,
-//!         #[influxdb(tag)] wind_direction: String,
+//!         #[influxdb(tag)]
+//!         wind_direction: String,
 //!     }
 //!
 //!     // Let's write some data into a measurement called `weather`
-//!     let weather_readings = vec!(
+//!     let weather_readings = vec![
 //!         WeatherReading {
 //!             time: Timestamp::Hours(1).into(),
 //!             humidity: 30,
 //!             wind_direction: String::from("north"),
-//!         }.into_query("weather"),
+//!         }
+//!         .into_query("weather"),
 //!         WeatherReading {
 //!             time: Timestamp::Hours(2).into(),
 //!             humidity: 40,
 //!             wind_direction: String::from("west"),
-//!         }.into_query("weather"),
-//!     );
+//!         }
+//!         .into_query("weather"),
+//!     ];
 //!
-//!     if let Err(e) = client.query(weather_readings).await {
-//!         println!("Error writing result: {e}");
-//!         return;
-//!     }
+//!     client.query(weather_readings).await?;
 //!
 //!     // Let's see if the data we wrote is there
 //!     let read_query = ReadQuery::new("SELECT * FROM weather");
 //!
-//!     match client.query(read_query).await {
-//!         Ok(read_result) => println!("{}", read_result),
-//!         Err(e) => println!("Error reading result: {e}"),
-//!     }
+//!     let read_result = client.query(read_query).await?;
+//!     println!("{}", read_result);
+//!     Ok(())
 //! }
 //! ```
 //!
