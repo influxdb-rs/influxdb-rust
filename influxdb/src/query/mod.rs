4a93896c2cefee5d3fe898311ff9afe937525286
Andrey Minin <45432000+amonin7@users.noreply.github.com>
2024-02-14 10:56:17 +0100

    Fix bug with conversion of Timestamp::Microseconds to chrono::Datetime (#134)

--- influxdb/src/query/mod.rs
+++ influxdb/src/query/mod.rs
@@ -30,7 +30,9 @@ pub mod write_query;
 use std::fmt;
 
 use crate::{Error, ReadQuery, WriteQuery};
-use consts::{MILLIS_PER_SECOND, MINUTES_PER_HOUR, NANOS_PER_MILLI, SECONDS_PER_MINUTE};
+use consts::{
+    MILLIS_PER_SECOND, MINUTES_PER_HOUR, NANOS_PER_MICRO, NANOS_PER_MILLI, SECONDS_PER_MINUTE,
+};
 
 #[cfg(feature = "derive")]
 pub use influxdb_derive::InfluxDbWriteable;
@@ -76,8 +78,8 @@ impl From<Timestamp> for DateTime<Utc> {
                 Utc.timestamp_nanos(nanos.try_into().unwrap())
             }
             Timestamp::Nanoseconds(nanos) => Utc.timestamp_nanos(nanos.try_into().unwrap()),
-            Timestamp::Microseconds(mis) => {
-                let nanos = mis / 10000;
+            Timestamp::Microseconds(micros) => {
+                let nanos = micros * NANOS_PER_MICRO;
                 Utc.timestamp_nanos(nanos.try_into().unwrap())
             }
         }
@@ -230,7 +232,7 @@ pub enum QueryType {
 #[cfg(test)]
 mod tests {
     use super::consts::{
-        MICROS_PER_NANO, MILLIS_PER_SECOND, MINUTES_PER_HOUR, NANOS_PER_MILLI, SECONDS_PER_MINUTE,
+        MILLIS_PER_SECOND, MINUTES_PER_HOUR, NANOS_PER_MICRO, NANOS_PER_MILLI, SECONDS_PER_MINUTE,
     };
     use crate::query::{Timestamp, ValidQuery};
     use chrono::prelude::{DateTime, TimeZone, Utc};
@@ -301,9 +303,9 @@ mod tests {
     }
     #[test]
     fn test_chrono_datetime_from_timestamp_micros() {
-        let datetime_from_timestamp: DateTime<Utc> = Timestamp::Microseconds(1).into();
+        let datetime_from_timestamp: DateTime<Utc> = Timestamp::Microseconds(2).into();
         assert_eq!(
-            Utc.timestamp_nanos((1 / MICROS_PER_NANO).try_into().unwrap()),
+            Utc.timestamp_nanos((2 * NANOS_PER_MICRO).try_into().unwrap()),
             datetime_from_timestamp
         )
     }
