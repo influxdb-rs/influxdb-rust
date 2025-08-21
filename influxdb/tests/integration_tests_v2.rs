ff46a3e07f747c90874a28d52c957704000d88bc
Gero Gerke <hello@gero.dev>
2025-08-21 17:05:54 +0200

    parametrize influxdb client version

--- influxdb/tests/integration_tests_v2.rs
+++ influxdb/tests/integration_tests_v2.rs
@@ -15,7 +15,9 @@ use influxdb::{Client, Error, ReadQuery, Timestamp};
 async fn test_authed_write_and_read() {
     run_test(
         || async move {
-            let client = Client::new("http://127.0.0.1:2086", "mydb").with_token("admintoken");
+            use influxdb::InfluxVersion2;
+
+            let client: Client<InfluxVersion2> = Client::new("http://127.0.0.1:2086", "mydb").with_token("admintoken");
             let write_query = Timestamp::Hours(11)
                 .into_query("weather")
                 .add_field("temperature", 82);
@@ -31,7 +33,9 @@ async fn test_authed_write_and_read() {
             );
         },
         || async move {
-            let client = Client::new("http://127.0.0.1:2086", "mydb").with_token("admintoken");
+            use influxdb::InfluxVersion2;
+
+            let client: Client<InfluxVersion2> = Client::new("http://127.0.0.1:2086", "mydb").with_token("admintoken");
             let read_query = ReadQuery::new("DROP MEASUREMENT \"weather\"");
             let read_result = client.query(read_query).await;
             assert_result_ok(&read_result);
@@ -51,7 +55,9 @@ async fn test_wrong_authed_write_and_read() {
 
     run_test(
         || async move {
-            let client = Client::new("http://127.0.0.1:2086", "mydb").with_token("falsetoken");
+            use influxdb::InfluxVersion2;
+
+            let client: Client<InfluxVersion2> = Client::new("http://127.0.0.1:2086", "mydb").with_token("falsetoken");
             let write_query = Timestamp::Hours(11)
                 .into_query("weather")
                 .add_field("temperature", 82);
@@ -91,7 +97,9 @@ async fn test_non_authed_write_and_read() {
 
     run_test(
         || async move {
-            let non_authed_client = Client::new("http://127.0.0.1:2086", "mydb");
+            use influxdb::InfluxVersion2;
+
+            let non_authed_client: Client<InfluxVersion2> = Client::new("http://127.0.0.1:2086", "mydb");
             let write_query = Timestamp::Hours(11)
                 .into_query("weather")
                 .add_field("temperature", 82);
